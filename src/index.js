056f7f5873c95120da535b49128cf9b3f946c818
huichuanf <154733651+huichuanf@users.noreply.github.com>
2024-07-12 10:25:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.huichaunf.top": dockerHub,
+  "quay.huichaunf.top": "https://quay.io",
+  "gcr.huichaunf.top": "https://gcr.io",
+  "k8s-gcr.huichaunf.top": "https://k8s.gcr.io",
+  "k8s.huichaunf.top": "https://registry.k8s.io",
+  "ghcr.huichaunf.top": "https://ghcr.io",
+  "cloudsmith.huichaunf.top": "https://docker.cloudsmith.io",
+  "ecr.huichaunf.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.huichaunf.top": dockerHub,
 };
 
 function routeByHosts(host) {
